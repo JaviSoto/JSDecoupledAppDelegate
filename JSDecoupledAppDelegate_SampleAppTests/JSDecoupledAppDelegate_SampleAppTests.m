71b91cb7bd9815543f35d20530b7702e3948d636
Javier Soto <javier.api@gmail.com>
2013-09-09 23:07:50 -0700

    Sample app

---
+++ JSDecoupledAppDelegate_SampleAppTests/JSDecoupledAppDelegate_SampleAppTests.m
@@ -0,0 +1,34 @@
+//
+//  JSDecoupledAppDelegate_SampleAppTests.m
+//  JSDecoupledAppDelegate_SampleAppTests
+//
+//  Created by Javier Soto on 9/9/13.
+//  Copyright (c) 2013 JavierSoto. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface JSDecoupledAppDelegate_SampleAppTests : XCTestCase
+
+@end
+
+@implementation JSDecoupledAppDelegate_SampleAppTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
